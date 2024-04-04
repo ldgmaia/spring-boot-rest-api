0b8508b7bc19b8e772b22e891316a8468775d822
Diego Maia <ldgmaia@gmail.com>
2024-04-04 11:06:13 -0400

    Feat: Spring Security configuration

--- src/main/java/com/example/api/domain/users/User.java
+++ src/main/java/com/example/api/domain/users/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String user;
+    private String username;
 
     private String password;
 }
