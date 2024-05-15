ae4f3bb4d2c1149f20767f5a7d420f1f868117a6
Diego Maia <ldgmaia@gmail.com>
2024-05-15 15:35:44 -0400

    Fix: categories related table

---
+++ src/main/resources/db/migration/V3.0__create-tables-for-categories.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS category_groups (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    needs_post bit,
+    needs_serial_number bit,
+    category_group_id BIGINT UNSIGNED,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    INDEX (category_group_id),
+    FOREIGN KEY (category_group_id) REFERENCES category_groups (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS category_components (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    child_category_id BIGINT UNSIGNED,
+    parent_category_id BIGINT UNSIGNED,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    INDEX (child_category_id),
+    INDEX (parent_category_id),
+    UNIQUE uc_child_category_id_parent_category_id (child_category_id, parent_category_id),
+    FOREIGN KEY (parent_category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (child_category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS category_fields (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    data_level VARCHAR(255) NOT NULL,
+    category_id BIGINT UNSIGNED,
+    fields_id BIGINT UNSIGNED,
+    is_mandatory BIT,
+    order_on_label BIGINT,
+    print_on_label BIT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    INDEX (category_id),
+    INDEX (fields_id),
+    FOREIGN KEY (fields_id) REFERENCES fields (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
