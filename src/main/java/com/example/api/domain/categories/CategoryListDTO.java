03bdda4221a182a099f2d96cdb47a3cf5ee7b498
Diego Maia <ldgmaia@gmail.com>
2024-10-17 14:28:49 -0400

    WIP: Category adjustments

--- src/main/java/com/example/api/domain/categories/CategoryListDTO.java
+++ src/main/java/com/example/api/domain/categories/CategoryListDTO.java
@@ -1,8 +1,8 @@
 package com.example.api.domain.categories;
 
-public record CategoryListDTO(Long id, String name, Boolean enabled, Boolean needsPost) {
+public record CategoryListDTO(Long id, String name, Boolean enabled, Boolean needsPost, Boolean needsSerialNumber) {
 
     public CategoryListDTO(Category category) {
-        this(category.getId(), category.getName(), category.getEnabled(), category.getNeedsPost());
+        this(category.getId(), category.getName(), category.getEnabled(), category.getNeedsPost(), category.getNeedsSerialNumber());
     }
 }
