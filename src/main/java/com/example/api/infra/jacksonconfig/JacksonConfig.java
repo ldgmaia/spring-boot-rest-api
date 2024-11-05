e2f591d0710dc9ca0a08875ca76c0aa4c73eb6fc
Diego Maia <ldgmaia@gmail.com>
2024-11-05 09:08:21 -0500

    Infra: Enable Spring Data Web Support

---
+++ src/main/java/com/example/api/infra/jacksonconfig/JacksonConfig.java
@@ -0,0 +1,11 @@
+package com.example.api.infra.jacksonconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
+public class JacksonConfig {
+}
